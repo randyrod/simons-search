3bd23f885b8cbc96cbd6e3c82b78ef827a9ece98
RandyRod <randyro91@gmail.com>
2020-12-19 16:44:44 +0100

    Register repositories in services container

--- src/SimonsSearch.Api/Startup.cs
+++ src/SimonsSearch.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SimonsSearch.Core.Repositories;
 
 namespace SimonsSearch.Api
 {
@@ -26,6 +27,11 @@ namespace SimonsSearch.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddScoped<IBuildingRepository, BuildingRepository>();
+            services.AddScoped<ILockRepository, LockRepository>();
+            services.AddScoped<IGroupRepository, GroupRepository>();
+            services.AddScoped<IMediumRepository, MediumRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
