aca52cd5c0388155b448e4e8a5fb43d1e39895b9
RandyRod <randyro91@gmail.com>
2020-12-21 23:05:54 +0100

    Update lock service repository to be able to query for lock types

--- src/SimonsSearch.Core/Repositories/ILockRepository.cs
+++ src/SimonsSearch.Core/Repositories/ILockRepository.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
+using SimonsSearch.Common.Enums;
 using SimonsSearch.Data.Entities;
 
 namespace SimonsSearch.Core.Repositories
 {
     public interface ILockRepository
     {
-        IReadOnlyList<Lock> GetLocksMatchingTerms(IReadOnlyList<string> searchTerms);
+        IReadOnlyList<Lock> GetLocksMatchingTerms(IReadOnlyList<string> searchTerms, IReadOnlyList<LockType> lockTypes);
     }
 }
--- src/SimonsSearch.Core/Repositories/LockRepository.cs
+++ src/SimonsSearch.Core/Repositories/LockRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using SimonsSearch.Common.Enums;
 using SimonsSearch.Data;
 using SimonsSearch.Data.Entities;
 
@@ -10,18 +11,19 @@ namespace SimonsSearch.Core.Repositories
     {
         private readonly SimonsSearchDbContext _dbContext;
 
-        public LockRepository(SimonsSearchDbContext dbContext)
+        public LockRepository()
         {
-            _dbContext = dbContext;
+            _dbContext = new SimonsSearchDbContext();
         }
 
-        public IReadOnlyList<Lock> GetLocksMatchingTerms(IReadOnlyList<string> searchTerms) =>
+        public IReadOnlyList<Lock> GetLocksMatchingTerms(IReadOnlyList<string> searchTerms, IReadOnlyList<LockType> lockTypes) =>
             _dbContext.Locks.Where(x => searchTerms.Any(s =>
-                    x.Name.Contains(s, StringComparison.InvariantCultureIgnoreCase) ||
-                    x.Description.Contains(s, StringComparison.InvariantCultureIgnoreCase) ||
-                    x.Floor.Contains(s, StringComparison.InvariantCultureIgnoreCase) ||
-                    x.RoomNumber.Contains(s) ||
-                    x.SerialNumber.Contains(s)))
+                    (x.Name?.Contains(s, StringComparison.InvariantCultureIgnoreCase) ?? false) ||
+                    (x.Description?.Contains(s, StringComparison.InvariantCultureIgnoreCase) ?? false) ||
+                    (x.Floor?.Contains(s, StringComparison.InvariantCultureIgnoreCase) ?? false) ||
+                    (x.RoomNumber?.Contains(s, StringComparison.InvariantCultureIgnoreCase) ?? false) ||
+                    (x.SerialNumber?.Contains(s, StringComparison.InvariantCultureIgnoreCase) ?? false) ||
+                    lockTypes.Any(t => t == x.Type)))
                 .Distinct()
                 .ToList();
     }
